2cf7fe84cb211c75f21f5ca90b5f44c95b3c23a8
SJimmyW <163605349+SJimmyW@users.noreply.github.com>
2025-04-26 20:44:41 -0300

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
 # install.R
-install.packages(c("shiny","dplyr","ggplot2","lubridate","writexl","readxl", "shiny", "ggplot2"))
+install.packages(c("shiny","dplyr","ggplot2","lubridate","writexl","readxl", "shiny", "ggplot2", "rsconnect"))
